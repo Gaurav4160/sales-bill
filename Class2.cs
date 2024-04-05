c1a4444ee2bcd5949c3cb2749f30a85609d0217c
Gaurav4160 <146840181+Gaurav4160@users.noreply.github.com>
2024-04-05 13:24:37 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,48 @@
+﻿// Default.aspx.cs
+
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Load the sales bills on page load
+            LoadSalesBills();
+        }
+    }
+
+    private void LoadSalesBills()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionString"].ConnectionString;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT SaleID, BillDate, CustomerName FROM Sales_Head";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable salesTable = new DataTable();
+            adapter.Fill(salesTable);
+
+            gvSalesBills.DataSource = salesTable;
+            gvSalesBills.DataBind();
+        }
+    }
+
+    protected void btnAddSale_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AddSale.aspx");
+    }
+
+    protected void gvSalesBills_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "ViewSale")
+        {
+            int saleID = Convert.ToInt32(e.CommandArgument);
+            Response.Redirect("ViewSale.aspx?SaleID=" + saleID);
+        }
+    }
+}
---
+++ SqlQuery_1.sql
@@ -0,0 +1,29 @@
+﻿-- Create the Products table
+CREATE TABLE Products (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    ProductName NVARCHAR(50),
+    UnitPrice DECIMAL,
+    StockQuantity DECIMAL
+);
+
+-- Create the Sales_Head table
+CREATE TABLE Sales_Head (
+    SaleID INT IDENTITY(1,1) PRIMARY KEY,
+    BillDate DATETIME,
+    CustomerName NVARCHAR(100),
+    Mob NVARCHAR(20),
+    Address TEXT
+);
+
+-- Create the Sales_Line table
+CREATE TABLE Sales_Line (
+    SaleID INT,
+    LineId INT,
+    ProductID INT,
+    Quantity INT,
+    UnitPrice DECIMAL,
+    TotalPrice DECIMAL,
+    PRIMARY KEY (SaleID, LineId),
+    FOREIGN KEY (SaleID) REFERENCES Sales_Head(SaleID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
